62cffd74c379210dba9767dfac075aaf5d5f55a9
mufasa-dev <bruno.ribeiro96@hotmail.com.br>
2025-05-03 09:52:08 -0300

    feat: login with bcrypt

--- handlers/auth.go
+++ handlers/auth.go
@@ -2,17 +2,20 @@ package handlers
 
 import (
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v4"
+	"github.com/mufasa-dev/Wallet-flow-api-in-Golang/schemas"
+	"golang.org/x/crypto/bcrypt"
 )
 
-var jwtSecret = []byte("my-secret-123")
+var jwtSecret = os.Getenv("JWT_SECRET")
 
 type LoginRequest struct {
-	Username string `json:"username`
-	Password string `json:"password`
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
 func Login(c *gin.Context) {
@@ -22,21 +25,74 @@ func Login(c *gin.Context) {
 		return
 	}
 
-	if req.Username != "admin" || req.Password != "1234" {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+	var storedUser schemas.User
+	result := db.Where("Name = ?", req.Username).Find(&storedUser)
+	if result.Error != nil || storedUser.ID == 0 {
+		sendError(c, http.StatusNotFound, "user not found")
 		return
 	}
 
+	if !CheckPasswordHash(req.Password, storedUser.Password) {
+		sendError(c, http.StatusNotFound, "Incorrect password")
+		return
+	}
+
+	token, err := GenetareJWT(req.Username)
+	if err != nil {
+		sendError(c, http.StatusInternalServerError, "Failed to generate token")
+		return
+	}
+
+	sendSuccess(c, "sigin", gin.H{"token": token})
+}
+
+func CreateUserHandler(ctx *gin.Context) {
+	request := CreateUserRequest{}
+
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	hashedPassword, err := HashPassword(request.Password)
+	if err != nil {
+		sendError(ctx, http.StatusInternalServerError, "Error processing password")
+		return
+	}
+
+	user := schemas.User{
+		Name:     request.Name,
+		Password: hashedPassword,
+		CPF:      request.CPF,
+		Wallet:   request.Wallet,
+	}
+
+	if err := db.Create(&user).Error; err != nil {
+		logger.Errorf("error create opening %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	sendSuccess(ctx, "create-user", user)
+}
+
+func GenetareJWT(username string) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"username": req.Username,
+		"username": username,
 		"exp":      time.Now().Add(time.Hour * 24).Unix(),
 	})
+	return token.SignedString((jwtSecret))
+}
 
-	tokenString, err := token.SignedString(jwtSecret)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to gendrate token"})
-		return
-	}
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(hashedPassword), err
+}
 
-	c.JSON(http.StatusOK, gin.H{"token": tokenString})
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
